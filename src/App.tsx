7f6f91cf7e28bec4e8f63a7d6931069fb5f2045f
Matthijs Groen <matthijs.groen@gmail.com>
2023-06-30 18:41:34 +0200

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
 import { army, village } from "./testScenarios/simpleVillage";
 import { Combat } from "./components/Combat";
 import { useState } from "react";
-import { PathFinding } from "./debug/Pathfinding";
 
 const App = () => {
-  const [popupActive, setActivePopup] = useState<
-    null | "attack" | "pathFinding"
-  >(null);
+  const [popupActive, setActivePopup] = useState<null | "attack">(null);
 
   return (
     <div>
       {popupActive === "attack" && <Combat base={village} army={army} />}
-      {popupActive === "pathFinding" && <PathFinding base={village} />}
 
       <button onClick={() => setActivePopup("attack")}>Attack</button>
-      <button onClick={() => setActivePopup("pathFinding")}>
-        PathFinding test
-      </button>
     </div>
   );
 };
