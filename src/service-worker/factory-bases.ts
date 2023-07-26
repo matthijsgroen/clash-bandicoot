6464e0c97eb85116dac8e71ef850ae5c900c2bc7
Matthijs Groen <matthijs.groen@gmail.com>
2023-07-26 16:46:43 +0200

    Update layout name

--- src/service-worker/factory-bases.ts
+++ src/service-worker/factory-bases.ts
@@ -1,6 +1,6 @@
 export const factoryBases = [
   {
-    name: "Training level 1",
+    name: "Giant Smash",
     layout:
       "eNotjdENwjAQQ32J7-oW2qSVUokFQGIGxmD_VeCQ-LOf5Gd98K6mwEvROO8FZ2d1dcakRuJk5u1Hs9aZGK5J14KxOJ6ulUbctkKl5N7oR8F-KbBORk65eKenKeTCsRKPRlHVMNJmCe3_Q30BDU8F4Q",
   },
