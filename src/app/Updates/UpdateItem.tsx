cf161f3ea522681159509df24cfbb91a9c02e65e
Matthijs Groen <matthijs.groen@gmail.com>
2023-09-15 16:20:05 +0200

    Improve update display

--- src/app/Updates/UpdateItem.tsx
+++ src/app/Updates/UpdateItem.tsx
@@ -2,35 +2,31 @@ import { Fragment } from "react";
 import { Update, categories } from "../../data/changes/type";
 import { Text } from "../../ui-components/atoms/Text";
 
-export const UpdateItem: React.FC<{ update: Update }> = ({ update }) => {
-  const date = new Date(update.date);
-
-  return (
-    <div key={update.date}>
-      <Text element="h1" size="large">
-        Update:{" "}
-        {date.toLocaleString("en", {
-          day: "numeric",
-          month: "long",
-          year: "numeric",
-        })}
-      </Text>
-      {categories
-        .filter((c) => !!update.changes[c])
-        .map((c) => (
-          <Fragment key={c}>
-            <Text element="h2">{c}</Text>
-            <ul>
-              {update.changes[c]?.map((item) => (
-                <li key={item}>
-                  <Text skipOutline color="black">
-                    {item}
-                  </Text>
-                </li>
-              ))}
-            </ul>
-          </Fragment>
-        ))}
-    </div>
-  );
-};
+export const UpdateItem: React.FC<{ update: Update }> = ({ update }) => (
+  <div key={update.date}>
+    <Text element="h2" size="large">
+      Update:{" "}
+      {new Date(update.date).toLocaleString("en", {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+      })}
+    </Text>
+    {categories
+      .filter((c) => !!update.changes[c])
+      .map((c) => (
+        <Fragment key={c}>
+          <Text element="h3">{c}</Text>
+          <ul>
+            {update.changes[c]?.map((item) => (
+              <li key={item}>
+                <Text skipOutline color="black">
+                  {item}
+                </Text>
+              </li>
+            ))}
+          </ul>
+        </Fragment>
+      ))}
+  </div>
+);
